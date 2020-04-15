06d19aa6739fdd1122499e4f1f9ff8c17363508a
Sonali <sonali_mantri08@yahoo.co.in>
2020-04-15 16:12:41 +1000

    Adding Addition.java file.

---
+++ src/main/java/com/addition/Addition.java
@@ -0,0 +1,15 @@
+package com.addition;
+
+public class Addition {
+
+	public static int a = 10;
+	public static int b = 20;
+	public static int addition_result;
+
+	public static void main(String[] args) {
+		addition_result = a + b;
+		System.out.println("Result of addition is: " + addition_result);
+
+	}
+
+}
